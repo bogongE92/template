da860b94579eed7a307a1038fa37c68d226eb253
Lena <112360499+bogongE92@users.noreply.github.com>
2024-05-10 16:47:48 +0900

    ESlint 설정

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,28 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
 
 // page 컴포넌트 정보 가져오기
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/HomeView.vue';
 
 // 라우츠로 맵핑해주기
 /* const routes = [] */
 
-
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: [
+		{
+			path: '/',
+			name: 'home',
+			component: HomeView,
+		},
+		{
+			path: '/about',
+			name: 'about',
+			// route level code-splitting
+			// this generates a separate chunk (About.[hash].js) for this route
+			// which is lazy-loaded when the route is visited.
+			component: () => import('../views/AboutView.vue'),
+		},
+	],
+});
 
-export default router
+export default router;
